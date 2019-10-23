aa2f13a0b88540887e3c58cc100b6080442762fc
Hasham Ahmad <mr.hash@gmail.com>
2019-10-23 17:26:58 +0400

    Add scrolling support

--- src/Storage/Elasticsearch7StorageAdapter.php
+++ src/Storage/Elasticsearch7StorageAdapter.php
@@ -10,17 +10,22 @@ declare(strict_types=1);
 
 namespace Daikon\Elasticsearch7\Storage;
 
+use Assert\Assertion;
 use Daikon\Dbal\Exception\DbalException;
 use Daikon\Elasticsearch7\Connector\Elasticsearch7Connector;
 use Daikon\Metadata\Metadata;
 use Daikon\ReadModel\Projection\ProjectionMap;
 use Daikon\ReadModel\Query\QueryInterface;
+use Daikon\ReadModel\Storage\ScrollAdapterInterface;
 use Daikon\ReadModel\Storage\SearchAdapterInterface;
 use Daikon\ReadModel\Storage\StorageAdapterInterface;
 use Daikon\ReadModel\Storage\StorageResultInterface;
 use Elasticsearch\Common\Exceptions\Missing404Exception;
 
-final class Elasticsearch7StorageAdapter implements StorageAdapterInterface, SearchAdapterInterface
+final class Elasticsearch7StorageAdapter implements
+    StorageAdapterInterface,
+    SearchAdapterInterface,
+    ScrollAdapterInterface
 {
     /** @var Elasticsearch7Connector */
     private $connector;
@@ -78,25 +83,80 @@ final class Elasticsearch7StorageAdapter implements StorageAdapterInterface, Sea
             'index' => $this->getIndex(),
             'from' => $from,
             'size' => $size,
-            'body' => $query->toNative()
+            'body' => $query->toNative(),
+            'rest_total_hits_as_int' => true
         ]);
 
         $results = $this->connector->getConnection()->search($query);
 
-        $projections = [];
-        foreach ($results['hits']['hits'] as $document) {
-            $projectionClass = $document['_source']['@type'];
-            $projections[$document['_id']] = $projectionClass::fromNative($document['_source']);
-        }
+        return new Elasticsearch7StorageResult(
+            $this->makeProjectionMap($results['hits']['hits']),
+            Metadata::fromNative(['total' => $results['hits']['total']])
+        );
+    }
+
+    public function scrollStart(QueryInterface $query, int $size = null, $cursor = null): StorageResultInterface
+    {
+        $query = array_merge($this->settings['search'] ?? [], [
+            'index' => $this->getIndex(),
+            'size' => $size,
+            'body' => $query->toNative(),
+            'scroll' => $this->settings['scroll_timeout'] ?? '1m',
+            'sort' => ['_doc'],
+            'rest_total_hits_as_int' => true
+        ]);
+
+        $results = $this->connector->getConnection()->search($query);
 
         return new Elasticsearch7StorageResult(
-            new ProjectionMap($projections),
-            Metadata::fromNative(['total' => $results['hits']['total']['value']])
+            $this->makeProjectionMap($results['hits']['hits']),
+            Metadata::fromNative([
+                'total' => $results['hits']['total'],
+                'cursor' => $results['_scroll_id']
+            ])
         );
+    }
+
+    public function scrollNext($cursor, int $size = null): StorageResultInterface
+    {
+        Assertion::string($cursor);
+        Assertion::notEmpty($cursor);
+
+        $results = $this->connector->getConnection()->scroll([
+            'scroll_id' => $cursor,
+            'scroll' => $this->settings['scroll_timeout'] ?? '1m',
+            'rest_total_hits_as_int' => true
+        ]);
+
+        return new Elasticsearch7StorageResult(
+            $this->makeProjectionMap($results['hits']['hits']),
+            Metadata::fromNative([
+                'total' => $results['hits']['total'],
+                'cursor' => $results['_scroll_id']
+            ])
+        );
+    }
+
+    public function scrollEnd($cursor): void
+    {
+        Assertion::string($cursor);
+        Assertion::notEmpty($cursor);
+
+        $this->connector->getConnection()->clearScroll(['scroll_id' => $cursor]);
     }
 
     private function getIndex(): string
     {
         return $this->settings['index'] ?? $this->connector->getSettings()['index'];
+    }
+
+    private function makeProjectionMap(array $documents): ProjectionMap
+    {
+        $projections = [];
+        foreach ($documents as $document) {
+            $projectionClass = $document['_source']['@type'];
+            $projections[$document['_id']] = $projectionClass::fromNative($document['_source']);
+        }
+        return new ProjectionMap($projections);
     }
 }
